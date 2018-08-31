ed4edae38405b9cc42d2161e7cbf19ccfa79df6b
tomas <tomas.mccandless@gmail.com>
2018-08-31 11:51:29 -0700

    Artifact id suffix and cross compilation (#3)
    * add scoverage, use gradle 4.8.1, update nebula versions
    
    * publishing and signing build logic
    
    * fake javadoc
    
    * set groupId in buildscript
    
    * multi scala version
    
    * additional comments
    
    * minor cleanup
    
    * dont require signing for local publish
    
    * update readme
    
    * update gradle.properties
    
    * update gradle.properties
    
    * publish script
    
    * publish script
    
    * publish script
    
    * publish script

---
+++ publish.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+set -e
+
+# usage: ./publish.sh <release type> <release scope> <repository>
+#   release type must be one of { devSnapshot, snapshot, candidate, final }
+#   release scope must be one of { major, minor, patch }
+#   repository must be one of { local, sonatype }
+
+if [[ $# -ne 3 ]]
+then
+  echo "usage: ./publish.sh <release type> <release scope> <repository>"
+  exit 1
+fi
+
+RELEASE_TYPE=$1
+RELEASE_SCOPE=$2
+REPOSITORY=$3
+
+# check that we have a valid release scope
+if [[ $RELEASE_SCOPE != 'major' && $RELEASE_SCOPE != 'minor' && $RELEASE_SCOPE != 'patch' ]]
+then
+  echo "$RELEASE_SCOPE is not valid. (must be one of 'major', 'minor', or 'patch')."
+  exit 1
+fi
+
+# check that we have a valid repository
+if [[ $REPOSITORY = 'sonatype' ]]
+then
+  PUBLISH_TASK="publish"
+elif [[ $REPOSITORY = 'local' ]]
+then
+  PUBLISH_TASK="publishToMavenLocal"
+else
+  echo "$REPOSITORY is not valid. (must be one of 'sonatype' or 'local')."
+  exit 1
+fi
+
+
+if [[ $RELEASE_TYPE = 'final' || $RELEASE_TYPE = 'candidate' ]]
+then
+  # create the tag once, then cross-compile
+  echo "creating repo tag for $RELEASE_TYPE release"
+  ./gradlew -Prelease.scope=$RELEASE_SCOPE clean $RELEASE_TYPE
+  echo "publishing $REPOSITORY artifacts for $RELEASE_SCOPE $RELEASE_TYPE release"
+  ./gradlew -Prelease.useLastTag=true $PUBLISH_TASK
+elif [[ $RELEASE_TYPE = 'devSnapshot' || $RELEASE_TYPE == 'snapshot' ]]
+then
+  echo "publishing $REPOSITORY artifacts for $RELEASE_SCOPE $RELEASE_TYPE release. repo tag will not be created."
+  ./gradlew -Prelease.scope=$RELEASE_SCOPE clean $RELEASE_TYPE $PUBLISH_TASK
+else
+  echo "$RELEASE_TYPE is not a valid release type"
+  exit 1
+fi
