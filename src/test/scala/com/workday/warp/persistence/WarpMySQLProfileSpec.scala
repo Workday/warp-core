d50ade7990835cbb8ff191330eb96129077bd95d
Richie Wang <wang.ruiqi.94@gmail.com>
2019-05-14 16:18:07 -0700

    one more test

--- src/test/scala/com/workday/warp/persistence/WarpMySQLProfileSpec.scala
+++ src/test/scala/com/workday/warp/persistence/WarpMySQLProfileSpec.scala
@@ -1,10 +1,45 @@
 package com.workday.warp.persistence
 
+import com.workday.warp.common.category.UnitTest
 import com.workday.warp.common.spec.WarpJUnitSpec
-
+import org.junit.experimental.categories.Category
+import org.junit.{Before, Test}
+import Tables._
+import WarpMySQLProfile.api._
+import WarpMySQLProfileSpec._
 /**
   * Created by ruiqi.wang
   */
 class WarpMySQLProfileSpec extends WarpJUnitSpec with CorePersistenceAware {
+
+  /** Truncates the schema. */
+  @Before
+  def truncateSchema(): Unit = {
+    Connection.refresh()
+    CorePersistenceUtils.truncateSchema()
+  }
+
+  @Test
+  @Category(Array(classOf[UnitTest]))
+  def regexMatch(): Unit = {
+    this.persistenceUtils.findOrCreateTestDefinition(methodSignature1)
+    this.persistenceUtils.findOrCreateTestDefinition(methodSignature2)
+
+    val action = TestDefinition.filter(testDefinitions =>
+      testDefinitions.methodSignature regexLike "hello"
+    )
+
+    val rows: Seq[TestDefinitionRow] = this.persistenceUtils.runWithRetries(action.result, 5)
+    val check: Boolean = rows.exists(t => t.methodSignature.contains("hello") && !t.methodSignature.contains("bye"))
+
+    check shouldBe true
+  }
+
+}
+
+object WarpMySQLProfileSpec {
+
+  val methodSignature1 = "com.workday.warp.slick.implicits.hello"
+  val methodSignature2 = "com.workday.warp.slick.implicits.bye"
 
 }
