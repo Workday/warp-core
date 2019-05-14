22d45c479b54539e0eae3c2ec9e7a2e4824c945c
Richie Wang <wang.ruiqi.94@gmail.com>
2019-05-14 15:46:11 -0700

    proper testing. renamed some stuff

---
+++ src/test/scala/com/workday/warp/persistence/mysql/SlickExtensionMethodsSpec.scala
@@ -0,0 +1,65 @@
+package com.workday.warp.persistence.mysql
+
+import java.util.Date
+
+import com.workday.warp.common.category.UnitTest
+import com.workday.warp.common.spec.WarpJUnitSpec
+import com.workday.warp.persistence.Tables._
+import com.workday.warp.persistence.Tables.profile.api._
+import com.workday.warp.persistence.mysql.SlickExtensionMethodsSpec._
+import com.workday.warp.persistence.{Connection, CorePersistenceAware, CorePersistenceUtils}
+import org.junit.experimental.categories.Category
+import org.junit.{Before, Test}
+
+/**
+  * Created by ruiqi.wang
+  */
+class SlickExtensionMethodsSpec extends WarpJUnitSpec with CorePersistenceAware with WarpSlickImplicits {
+
+  /** Truncates the schema. */
+  @Before
+  def truncateSchema(): Unit = {
+    Connection.refresh()
+    CorePersistenceUtils.truncateSchema()
+  }
+
+  /**
+    * Checks that our custom standard deviation aggregation function works correctly.
+    */
+  @Test
+  @Category(Array(classOf[UnitTest]))
+  def standardDeviation(): Unit = {
+    this.persistenceUtils.createTestExecution(methodSignature1, new Date, 1.0, 10)
+    this.persistenceUtils.createTestExecution(methodSignature1, new Date, 2.0, 10)
+    this.persistenceUtils.createTestExecution(methodSignature1, new Date, 3.0, 10)
+    this.persistenceUtils.createTestExecution(methodSignature2, new Date, 2.0, 10)
+    this.persistenceUtils.createTestExecution(methodSignature2, new Date, 4.0, 10)
+    this.persistenceUtils.createTestExecution(methodSignature2, new Date, 6.0, 10)
+
+    val query = TestExecution.groupBy(_.idTestDefinition)
+    val action = query.map { case (id, metrics) => (
+      id,
+      metrics.map(_.responseTime).std
+      )
+    }
+
+    val rows: Seq[(Int, Option[Double])] = this.persistenceUtils.runWithRetries(action.result, 5).sortBy(_._1)
+
+    rows.head._2 shouldBe defined
+    rows.head._2.get should equal (STD_1)
+    rows(1)._2 shouldBe defined
+    rows(1)._2.get should equal (STD_2)
+
+  }
+
+}
+
+object SlickExtensionMethodsSpec {
+
+  val methodSignature1 = "com.workday.warp.slick.implicits.test.1"
+  val methodSignature2 = "com.workday.warp.slick.implicits.test.2"
+
+  val STD_1: Double = 0.816496580927726
+  val STD_2: Double = 1.632993161855452
+
+}
