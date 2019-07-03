3668e377b7fff176f09a03960f813c26d6176af8
tomas.mccandless <tomas.mccandless@workday.com>
2019-07-03 13:38:44 -0700

    PropertyEntry cleanup

--- src/test/scala/com/workday/warp/logger/WarpLogUtilsSpec.scala
+++ src/test/scala/com/workday/warp/logger/WarpLogUtilsSpec.scala
@@ -15,7 +15,8 @@ class WarpLogUtilsSpec extends WarpJUnitSpec {
   @Category(Array(classOf[UnitTest]))
   def parseLogLevel(): Unit = {
 
-    WarpLogUtils.parseLevel("NOT_VALID_LEVEL", "DEBUG") should be (Level.DEBUG)
-    WarpLogUtils.parseLevel("NOT_VALID_LEVEL", "ALSO_NOT_VALID_LEVEL") should be (Level.INFO)
+    WarpLogUtils.parseLevel("NOT_VALID_LEVEL", Option("DEBUG")) should be (Level.DEBUG)
+    WarpLogUtils.parseLevel("NOT_VALID_LEVEL", Option("ALSO_NOT_VALID_LEVEL")) should be (Level.INFO)
+    WarpLogUtils.parseLevel("NOT_VALID_LEVEL", None) should be (Level.INFO)
   }
 }
