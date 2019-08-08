64dacb4b3466db0ab911e27eb2e72060fc0ed7d9
Eileen <32992287+el5846@users.noreply.github.com>
2019-08-08 11:29:28 -0700

    Overloaded Functions (#23)
    * Create SQL-Functions.md
    
    * Update SQL-Functions.md
    
    * Update SQL-Functions.md
    
    * Update SQL-Functions.md
    
    * Update SQL-Functions.md
    
    * Update SQL-Functions.md
    
    * Need to implement subtractDate
    
    * First request
    
    * Update SQL-Functions.md
    
    * Fixed changes
    
    * Missed one
    
    * Update SQL-Functions.md
    
    * Changed now()
    
    * Added in subdate and UNIX_TIMESTAMP, working
    
    * Missed type annotation
    
    * Update SQL-Functions.md
    
    * Update SQL-Functions.md
    
    * Added round number
    
    * One tiny import
    
    * Added documentation for timestamp and subdate
    
    * Fixed now
    
    * Am I making too many commits
    
    * Swear this is the last add on for this pull request - should I stop committing?
    
    * Update SQL-Functions.md
    
    * Put in range for timestamp, need to do now
    
    * Fixed now test
    
    * I can't believe someone wrote code to check for style but it's fixed now
    
    * Update SQL-Functions.md
    
    * Fixed capitalization
    
    * Update SQL-Functions.md
    
    * Added two overloaded methods
    
    * Added type
    
    * Update SQL-Functions.md
    
    * Overloaded roudn
    
    * Update SQL-Functions.md
    
    * Fixed comments
    
    * Entered in date timestamp
    
    * Added in type
    
    * Removed unnecessary
    
    * passes clean test
    
    * Fixed comments
    
    * Removed Minor Edit
    
    * Added in missing function
    
    * Update SQL-Functions.md
    
    * Changed ordering
    
    * Changed port back
    
    * Port
    
    * I screwed up
    
    * Delete SQL-Functions.md
    
    * Yeah I forgot to overload something
    
    * (auto) update dependency lock file, version=2.7.0-dev.69+Working.a415c00
    
    * renamed
    
    * (auto) update dependency lock file, version=2.7.0-dev.71+Working.35b3fcb
    
    * (auto) update dependency lock file, version=2.7.0-dev.72+Working.2f48da7
    
    * OOps
    
    * (auto) update dependency lock file, version=2.7.0-dev.74.uncommitted+Working.232ea6e
    
    * Not sure what happened
    
    * So many functions
    
    * comment

--- src/main/scala/com/workday/warp/persistence/mysql/HasWarpSlickDsl.scala
+++ src/main/scala/com/workday/warp/persistence/mysql/HasWarpSlickDsl.scala
@@ -79,7 +79,7 @@ trait HasWarpSlickDsl {
 
     /**
       * UNIX_TIMESTAMP operation - seconds elapsed since 1970-01-01 00:00:00 UTC
-      * Takes in a parameter of DATETIME: equivalent to UNIX_TIMESTAMP(DateTime)
+      * Takes in a parameter of date: equivalent to UNIX_TIMESTAMP(date)
       *
       * @return seconds in int
       */
@@ -131,6 +131,7 @@ trait HasWarpSlickDsl {
     /**
       * UNIX_TIMESTAMP operation - seconds elapsed since 1970-01-01 00:00:00 UTC
       * Takes in a parameter of TimeStamp: equivalent to UNIX_TIMESTAMP(date)
+
       * TODO: DATE and DATETIME parameters
       *
       * @return seconds in int
