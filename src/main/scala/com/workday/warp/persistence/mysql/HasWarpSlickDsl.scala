3c75dbf9ac0816ad5ac4a9952e75586b721fbae9
Eileen <32992287+el5846@users.noreply.github.com>
2019-08-08 11:29:28 -0700

    Overloaded Functions (#23)
    * Create SQL-Functions.md
    
    * Update SQL-Functions.md
    
    * Update SQL-Functions.md
    
    * Update SQL-Functions.md
    
    * Update SQL-Functions.md
    
    * Update SQL-Functions.md
    
    * Need to implement subtractDate
    
    * First request
    
    * Update SQL-Functions.md
    
    * Fixed changes
    
    * Missed one
    
    * Update SQL-Functions.md
    
    * Changed now()
    
    * Added in subdate and UNIX_TIMESTAMP, working
    
    * Missed type annotation
    
    * Update SQL-Functions.md
    
    * Update SQL-Functions.md
    
    * Added round number
    
    * One tiny import
    
    * Added documentation for timestamp and subdate
    
    * Fixed now
    
    * Am I making too many commits
    
    * Swear this is the last add on for this pull request - should I stop committing?
    
    * Update SQL-Functions.md
    
    * Put in range for timestamp, need to do now
    
    * Fixed now test
    
    * I can't believe someone wrote code to check for style but it's fixed now
    
    * Update SQL-Functions.md
    
    * Fixed capitalization
    
    * Update SQL-Functions.md
    
    * Added two overloaded methods
    
    * Added type
    
    * Update SQL-Functions.md
    
    * Overloaded roudn
    
    * Update SQL-Functions.md
    
    * Fixed comments
    
    * Entered in date timestamp
    
    * Added in type
    
    * Removed unnecessary
    
    * passes clean test
    
    * Fixed comments
    
    * Removed Minor Edit
    
    * Added in missing function
    
    * Update SQL-Functions.md
    
    * Changed ordering
    
    * Changed port back
    
    * Port
    
    * I screwed up
    
    * Delete SQL-Functions.md
    
    * Yeah I forgot to overload something
    
    * (auto) update dependency lock file, version=2.7.0-dev.69+Working.a415c00
    
    * renamed
    
    * (auto) update dependency lock file, version=2.7.0-dev.71+Working.35b3fcb
    
    * (auto) update dependency lock file, version=2.7.0-dev.72+Working.2f48da7
    
    * OOps
    
    * (auto) update dependency lock file, version=2.7.0-dev.74.uncommitted+Working.232ea6e
    
    * Not sure what happened
    
    * So many functions
    
    * comment

--- src/main/scala/com/workday/warp/persistence/mysql/HasWarpSlickDsl.scala
+++ src/main/scala/com/workday/warp/persistence/mysql/HasWarpSlickDsl.scala
@@ -1,6 +1,7 @@
 package com.workday.warp.persistence.mysql
 
 import java.sql.Timestamp
+
 import slick.jdbc.MySQLProfile.api._
 import slick.lifted.{Rep, SimpleExpression}
 
@@ -29,25 +30,67 @@ trait HasWarpSlickDsl {
       }
       expression.apply(str, pattern)
     }
-}
-  /**
-    * dsl class for math related operation
-    */
-  object NumberExtension {
+
     /**
-      * translates to Round(number, decimals)
-      * TODO: Implement truncation for 3 parameters
-      * @return number
+      * Correlates to "QUOTE()"
+      * @return properly SQL formatted escape string
       */
-    def round(number: Double, decimal: Int): Rep[Double] = {
-      val expression = SimpleExpression.binary[Double, Int, Double] { (number, decimal, queryBuilder) =>
-        queryBuilder.sqlBuilder += "ROUND ("
-        queryBuilder.expr(number)
-        queryBuilder.sqlBuilder += ","
-        queryBuilder.expr(decimal)
+    def quote(): Rep[String] = {
+      val expression = SimpleExpression.unary[String, String] { (str, queryBuilder) =>
+        queryBuilder.sqlBuilder += " QUOTE ("
+        queryBuilder.expr(str)
         queryBuilder.sqlBuilder += ")"
       }
-      expression.apply(number, decimal)
+      expression.apply(str)
+    }
+
+    /**
+      * Translates to YEAR(string) function
+      * @return just the year in Int
+      */
+    def year(): Rep[Int] = {
+      val expression = SimpleExpression.unary[String, Int] { (str, queryBuilder) =>
+        queryBuilder.sqlBuilder += " YEAR ("
+        queryBuilder.expr(str)
+        queryBuilder.sqlBuilder += ")"
+      }
+      expression.apply(str)
+    }
+
+    /**
+      * Translates to DATE(string) function
+      * @return just the date in string
+      */
+    def date(): Rep[String] = {
+      val expression = SimpleExpression.unary[String, String] { (str, queryBuilder) =>
+        queryBuilder.sqlBuilder += " DATE ("
+        queryBuilder.expr(str)
+        queryBuilder.sqlBuilder += ")"
+      }
+      expression.apply(str)
+    }
+}
+
+  /**
+    * dsl class for DateTime related operations
+    * @param date sql DateTime to perform operations on
+    */
+  implicit class DateExtensions(date: Rep[java.sql.Date]) {
+
+    /**
+      * UNIX_TIMESTAMP operation - seconds elapsed since 1970-01-01 00:00:00 UTC
+      * Takes in a parameter of date: equivalent to UNIX_TIMESTAMP(date)
+      *
+      * @return seconds in int
+      */
+    def unixTimestamp(): Rep[Long] = {
+      val expression = SimpleExpression.unary[java.sql.Date, Long] { (dateNode, queryBuilder) =>
+        queryBuilder.sqlBuilder += " UNIX_TIMESTAMP ("
+        queryBuilder.expr(dateNode)
+        queryBuilder.sqlBuilder += ")"
+      }
+
+      expression.apply(date)
     }
   }
 
@@ -73,19 +116,6 @@ trait HasWarpSlickDsl {
     }
 
     /**
-      * Translates to YEAR() function
-      * @return just the year in Int
-      */
-    def year(): Rep[Int] = {
-      val expression = SimpleExpression.unary[Timestamp, Int] { (timestamp, queryBuilder) =>
-        queryBuilder.sqlBuilder += " YEAR ("
-        queryBuilder.expr(timestamp)
-        queryBuilder.sqlBuilder += ")"
-      }
-      expression.apply(timestamp)
-    }
-
-    /**
       * Translates to DATE() function
       * @return just date in "yyyy-MM-dd"
       */
@@ -101,9 +131,8 @@ trait HasWarpSlickDsl {
     /**
       * UNIX_TIMESTAMP operation - seconds elapsed since 1970-01-01 00:00:00 UTC
       * Takes in a parameter of TimeStamp: equivalent to UNIX_TIMESTAMP(date)
-      *
-      * TODO: default empty parameter to calculate seconds based on current time, as well
-      * as DATE and DATETIME parameters
+
+      * TODO: DATE and DATETIME parameters
       *
       * @return seconds in int
       */
@@ -117,24 +146,34 @@ trait HasWarpSlickDsl {
     }
 
     /**
-      * SUBDATE operation: date - interval
-      * Equivalent to SUBDATE(date, INTERVAL expr unit)
-      *
-      * TODO: SUBDATE(expr, days)
-      *
-      * @return string date
+      * Translates to YEAR(timestamp) function
+      * @return just the year in Int
       */
-    def subdate(date: String, interval: String): Rep[String] = {
-      val expression = SimpleExpression.unary[String, String] { (date, queryBuilder) =>
+    def year(): Rep[Int] = {
+      val expression = SimpleExpression.unary[Timestamp, Int] { (timestamp, queryBuilder) =>
+        queryBuilder.sqlBuilder += " YEAR ("
+        queryBuilder.expr(timestamp)
+        queryBuilder.sqlBuilder += ")"
+      }
+      expression.apply(timestamp)
+    }
+
+    /**
+      * Translates to SUBDATE(timestamp) function
+      * @return just the date as string
+      */
+    def subdate(interval: String): Rep[String] = {
+      val expression = SimpleExpression.unary[Timestamp, String] { (timestamp, queryBuilder) =>
         queryBuilder.sqlBuilder += " subdate("
-        queryBuilder.expr(date)
+        queryBuilder.expr(timestamp)
         queryBuilder.sqlBuilder += ", INTERVAL "
         queryBuilder.sqlBuilder += interval
         queryBuilder.sqlBuilder += ")"
       }
-      expression.apply(date)
+      expression.apply(timestamp)
     }
-  }
+}
+
 
   object TimeStampExtensions {
 
@@ -148,5 +187,83 @@ trait HasWarpSlickDsl {
       }
     }
 
+    /**
+      * UNIX_TIMESTAMP operation - seconds elapsed since 1970-01-01 00:00:00 UTC
+      * @return seconds in int
+      */
+    def unixTimestamp(): Rep[Long] = {
+      SimpleExpression.nullary[Long] { (queryBuilder) =>
+        queryBuilder.sqlBuilder += " UNIX_TIMESTAMP ()"
+      }
+    }
+
+    /**
+      * SUBDATE operation: date - interval
+      * Equivalent to SUBDATE(date, INTERVAL expr unit)
+      *
+      * @return string date
+      */
+    def subdate(date: String, interval: String): Rep[String] = {
+      val expression = SimpleExpression.unary[String, String] { (date, queryBuilder) =>
+        queryBuilder.sqlBuilder += " subdate("
+        queryBuilder.expr(date)
+        queryBuilder.sqlBuilder += ", INTERVAL "
+        queryBuilder.sqlBuilder += interval
+        queryBuilder.sqlBuilder += ")"
+      }
+      expression.apply(date)
+    }
+
+    /**
+      * SUBDATE operation: date - days
+      * Equivalent to SUBDATE(date, days)
+      *
+      * @return string date
+      */
+    def subdate(date: String, amount: Int): Rep[String] = {
+      val expression = SimpleExpression.binary[String, Int, String] { (date, amount, queryBuilder) =>
+        queryBuilder.sqlBuilder += " subdate("
+        queryBuilder.expr(date)
+        queryBuilder.sqlBuilder += " , "
+        queryBuilder.expr(amount)
+        queryBuilder.sqlBuilder += ")"
+      }
+      expression.apply(date, amount)
+    }
   }
 }
+
+  /**
+    * dsl class for math related operation
+    */
+  object NumberExtension {
+    /**
+      * translates to Round(number, decimals)
+      * @return number
+      */
+    def round(number: Double, decimal: Int): Rep[Double] = {
+      val expression = SimpleExpression.binary[Double, Int, Double] { (number, decimal, queryBuilder) =>
+        queryBuilder.sqlBuilder += "ROUND ("
+        queryBuilder.expr(number)
+        queryBuilder.sqlBuilder += ","
+        queryBuilder.expr(decimal)
+        queryBuilder.sqlBuilder += ")"
+      }
+      expression.apply(number, decimal)
+    }
+
+    /**
+      * translates to Round(number)
+      * @return number
+      */
+    def round(number: Double): Rep[Int] = {
+      val expression = SimpleExpression.unary[Double, Int] { (number, queryBuilder) =>
+        queryBuilder.sqlBuilder += "ROUND ("
+        queryBuilder.expr(number)
+        queryBuilder.sqlBuilder += ")"
+      }
+      expression.apply(number)
+    }
+  }
+
+
