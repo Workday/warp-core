4dbf906f07ef557b39d44f70d22c14a2ac977716
tomas.mccandless <tomas.mccandless@workday.com>
2020-06-15 23:47:26 -0700

    modify build.gradle

--- src/test/scala/com/workday/warp/utils/AnnotationReaderSpec.scala
+++ src/test/scala/com/workday/warp/utils/AnnotationReaderSpec.scala
@@ -5,15 +5,12 @@ import com.workday.warp.common.annotation.ZScoreRequirement
 import com.workday.warp.common.category.UnitTest
 import com.workday.warp.common.spec.WarpJUnitSpec
 import com.workday.warp.common.utils.Implicits._
-import com.workday.warp.junit.RoundRobinExecution
 import org.junit.Test
 import org.junit.experimental.categories.Category
 
 /**
   * Created by tomas.mccandless on 6/8/18.
   */
-// only added for testing
-@RoundRobinExecution(invocations = 2)
 class AnnotationReaderSpec extends WarpJUnitSpec {
 
   class DoesNotHaveRoundRobin
@@ -30,19 +27,6 @@ class AnnotationReaderSpec extends WarpJUnitSpec {
     AnnotationReader.getRequiredMaxValue(this.getTestId) should be (-1 millis)
     AnnotationReader.getScheduleInvocations(this.getTestId) should be (1)
     AnnotationReader.getZScoreRequirement(this.getTestId) should be (ZScoreRequirement.DEFAULT_PERCENTILE)
-  }
-
-
-  /**
-    * Checks that we read the right round robin execution values.
-    */
-  @Test
-  @Category(Array(classOf[UnitTest]))
-  def roundRobin(): Unit = {
-    AnnotationReader.getRoundRobinExecution(this.getTestId) should be (2)
-    AnnotationReader.getRoundRobinExecution(this.getClass) should be (2)
-    AnnotationReader.getRoundRobinExecution(classOf[DoesNotHaveRoundRobin]) should be (1)
-    AnnotationReader.getRoundRobinExecution("this.class.does.not.exist") should be (1)
   }
 
 
