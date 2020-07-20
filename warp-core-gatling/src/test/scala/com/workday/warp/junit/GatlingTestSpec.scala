238330318c41bb2ac5b6c1768df361c908b27269
tomas.mccandless <tomas.mccandless@workday.com>
2020-07-20 15:26:48 -0700

    module isolation

---
+++ warp-core-gatling/src/test/scala/com/workday/warp/junit/GatlingTestSpec.scala
@@ -0,0 +1,17 @@
+package com.workday.warp.junit
+
+import com.workday.warp.adapters.gatling.WarpSimulation
+
+/**
+  * Created by tomas.mccandless on 7/1/20.
+  */
+class GatlingTestSpec extends WarpSimulation with GatlingTestExtensionLike // {
+
+//  val scn: ScenarioBuilder = scenario("Google test")
+//    .exec(http("Google")
+//      .get("https://www.google.comjdah")
+//      .check(status is 100))
+//
+//  val pop: PopulationBuilder = scn.inject(rampUsers(1) during FiniteDuration(1, TimeUnit.SECONDS))
+//  this.addAssertions(pop)
+// }
