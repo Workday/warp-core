2b21de1fabae322c1891d780c82da36036cf764a
tomas.mccandless <tomas.mccandless@workday.com>
2020-08-07 16:34:32 -0700

    additional docs

--- warp-core-gatling/src/test/scala/com/workday/warp/junit/GatlingTestSpec.scala
+++ warp-core-gatling/src/test/scala/com/workday/warp/junit/GatlingTestSpec.scala
@@ -16,10 +16,12 @@ import scala.concurrent.duration.FiniteDuration
 @RunWith(classOf[GatlingJUnitRunner])
 class GatlingTestSpec extends WarpSimulation {
 
+  // note that a test must implement its own assertions.
+  // even though the below url does not exist, the resulting unresolvable host exception does not fail the build.
   val scn: ScenarioBuilder = scenario("Google test")
     .exec(http("Google")
       .get("https://www.google.comjdah")
-      .check(status is 100))
+      .check(status is 200))
 
   val pop: PopulationBuilder = scn.inject(rampUsers(1) during FiniteDuration(1, TimeUnit.SECONDS))
   setUp(pop)
