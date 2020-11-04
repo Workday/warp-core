03e99874f3dd36dbe91ce85c615e113bf501dda5
tomas.mccandless <tomas.mccandless@workday.com>
2020-11-04 14:26:06 -0800

    more deprecations

--- warp-core-gatling/src/main/scala/com/workday/warp/adapters/gatling/GatlingJUnitRunner.scala
+++ warp-core-gatling/src/main/scala/com/workday/warp/adapters/gatling/GatlingJUnitRunner.scala
@@ -37,6 +37,7 @@ import scala.util.{Failure, Success, Try}
   * Copied from https://github.com/Pravoru/gatling-junitrunner. We couldn't use his artifact directly since it is not cross-compiled.
   * @param simulationClass
   */
+@deprecated("use junit5", since = "4.4.0")
 final class GatlingJUnitRunner(simulationClass: Class[_ <: Simulation]) extends Runner {
 
   /**
