905633be1311ec6382b4dda28bb6450dd6c1d53e
tomas.mccandless <tomas.mccandless@workday.com>
2020-11-06 17:31:17 -0800

    make TestId a case class

--- warp-core/src/main/scala/com/workday/warp/TestId.scala
+++ warp-core/src/main/scala/com/workday/warp/TestId.scala
@@ -3,6 +3,10 @@ package com.workday.warp
 import java.lang.reflect.Method
 
 import com.workday.warp.config.CoreConstants
+import com.workday.warp.utils.Implicits.{DecoratedOption, DecoratedOptional}
+import org.junit.jupiter.api.TestInfo
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.pmw.tinylog.Logger
 
 import scala.util.{Failure, Success, Try}
 
@@ -18,12 +22,7 @@ import scala.util.{Failure, Success, Try}
   *
   * Created by tomas.mccandless on 6/18/20.
   */
-// TODO make this a case class
-trait TestId {
-
-  def maybeTestClass: Try[Class[_]]
-
-  def maybeTestMethod: Try[Method]
+case class TestId(maybeTestClass: Try[Class[_]], maybeTestMethod: Try[Method]) {
 
   /**
     * Attempts to construct a fully qualified method name.
@@ -51,11 +50,39 @@ trait TestId {
 
 object TestId {
 
-  lazy val empty: TestId = new TestId {
-    override def maybeTestClass: Try[Class[_]] = Failure(new ClassNotFoundException)
-
-    override def maybeTestMethod: Try[Method] = Failure(new NoSuchMethodException)
-
+  lazy val empty: TestId = new TestId(Failure(new ClassNotFoundException()), Failure(new NoSuchMethodException())) {
     override lazy val maybeTestId: Try[String] = Success(CoreConstants.UNDEFINED_TEST_ID)
+  }
+
+  def fromTestInfo(info: TestInfo): TestId = TestId(info.getTestClass.toTry, info.getTestMethod.toTry)
+
+  def fromExtensionContext(context: ExtensionContext): TestId = TestId(context.getTestClass.toTry, context.getTestMethod.toTry)
+
+  /**
+    * Constructs a [[TestId]] from a fully qualified method signature.
+    *
+    * Attempts to parse a test class and test method from the signature, however note that there may be overloaded
+    * test methods.
+    *
+    * @param signature
+    * @return
+    */
+  def fromMethodSignature(signature: String): TestId = {
+    val className: String = signature take signature.lastIndexOf('.')
+    val methodName: String = signature drop signature.lastIndexOf('.') + 1
+
+    val maybeTestClass: Try[Class[_]] = Try(Class.forName(className))
+    val maybeTestMethod: Try[Method] = for {
+      cls <- maybeTestClass
+      methods: Array[Method] = cls.getMethods.filter(_.getName == methodName)
+      _ = if (methods.length > 1) {
+        Logger.warn(s"detected overloaded methods for signature $signature, annotation processing may not work as expected.")
+      }
+      method <- methods.headOption.toTry
+    } yield method
+
+    new TestId(maybeTestClass, maybeTestMethod) {
+      override lazy val maybeTestId: Try[String] = Success(signature)
+    }
   }
 }
