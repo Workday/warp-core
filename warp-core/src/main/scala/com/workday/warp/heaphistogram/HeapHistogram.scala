10c9f4677cd20e0bf8a501e99b42519604170dc7
vignesh.kalidas <vignesh.kalidas@workday.com>
2021-06-07 16:24:34 -0700

    Replace logger in HeapHistogram

--- warp-core/src/main/scala/com/workday/warp/heaphistogram/HeapHistogram.scala
+++ warp-core/src/main/scala/com/workday/warp/heaphistogram/HeapHistogram.scala
@@ -1,12 +1,12 @@
 package com.workday.warp.heaphistogram
 
-import org.pmw.tinylog.Logger
+import com.workday.warp.logger.WarpLogging
 
 /**
  * Container class for a heap histogram (a collection of heap histogram entries)
  *
  * Created by tomas.mccandless on 9/21/15.
  */
-class HeapHistogram(val histogramEntries: Seq[HeapHistogramEntry]) {
-  Logger.debug(s"created HeapHistogram with ${this.histogramEntries.length} entries.")
+class HeapHistogram(val histogramEntries: Seq[HeapHistogramEntry]) extends WarpLogging {
+  logger.debug(s"created HeapHistogram with ${this.histogramEntries.length} entries.")
 }
