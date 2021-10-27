d6c416f9a05f0f9e80dc161937cf6ec918d99ee7
tomas mccandless <tomas.mccandless@workday.com>
2021-10-27 15:41:14 -0700

    WIP macro expansion. still hardcoded type

--- warp-core/src/main/scala/com/workday/warp/monadic/Macros.scala
+++ warp-core/src/main/scala/com/workday/warp/monadic/Macros.scala
@@ -4,6 +4,7 @@ import com.workday.warp.monadic.WarpAlgebra.{WarpScript, interpretImpure}
 
 import language.experimental.macros
 import scala.reflect.macros.blackbox
+import scala.util.matching.Regex
 
 /**
  * Created by tomas.mccandless on 8/16/21.
@@ -31,15 +32,27 @@ object Macros {
   def generateTestIds[T](script: WarpScript[T]): WarpScript[T] = macro generateTestIdsImpl[T]
 
   def generateTestIdsImpl[T: ctx.WeakTypeTag](ctx: blackbox.Context)(script: ctx.Expr[WarpScript[T]]): ctx.Expr[WarpScript[T]] = {
+    // this untypecheck call is crucial to avoid crashing the compiler with mysterious errors like
+    // [Error] : Error while emitting MacrosSpec.scala
+    // value c
+    // https://github.com/scala/bug/issues/11628 is possibly related?
+    ctx.Expr[WarpScript[T]](testIdTransformer(ctx).transform(ctx.untypecheck(script.tree)))
+  }
+
+
+  /**
+    * Creates a Tree transformer that traverses
+    *
+    * @param ctx blackbox macro Context
+    * @return a (path-dependent) Tree transformer
+    */
+  def testIdTransformer(ctx: blackbox.Context): ctx.universe.Transformer = {
     import ctx.universe._
 
-    object TestIdTransformer extends Transformer {
+    new Transformer {
+      val enclosingClass: String = extractEnclosingClass(ctx)
       // TODO try to avoid using a var here
       var capturedArg: Option[Tree] = None
-      println(ctx.reifyEnclosingRuntimeClass.toString)
-      println(ctx.reifyEnclosingRuntimeClass.tpe)
-      println(ctx.reifyEnclosingRuntimeClass.symbol)
-      println(showRaw(ctx.reifyEnclosingRuntimeClass))
 
       override def transform(tree: Tree): Tree = tree match {
         // capture arg here, but we dont need to transform
@@ -52,37 +65,39 @@ object Macros {
         case Apply(func, _) if show(func).startsWith("com.workday.warp.monadic.WarpAlgebra.measure")
           && func.symbol.toString == "method measure" =>
 
-          println(extractEnclosingClass(ctx))
-          val testId: String = extractSyntheticMethodName(ctx)(capturedArg.get)
+          val testId: String = s"""$enclosingClass.${extractSyntheticMethodName(ctx)(capturedArg.get)}"""
           capturedArg = None
           println(s"""macro expansion: inserting testId "$testId" into expression "$tree"""")
           // TODO needs to work for any type
-          val lit = show(tree).replace(
+          val code = show(tree).replace(
             "com.workday.warp.monadic.WarpAlgebra.measure[Int](",
             s"""com.workday.warp.monadic.WarpAlgebra.measure[Int]("$testId", """
           )
-          // println(s"lit: $lit")
+          // println(s"code: $code")
           // quasiquote the transformed and parsed tree https://docs.scala-lang.org/overviews/quasiquotes/intro.html
-          q"${ctx.parse(lit)}"
+          q"${ctx.parse(code)}"
 
         case _ =>
           super.transform(tree)
       }
     }
-    // this untypecheck call is crucial to avoid crashing the compiler with mysterious errors like
-    // [Error] : Error while emitting MacrosSpec.scala
-    // value c
-    // https://github.com/scala/bug/issues/11628 is possibly related?
-    ctx.Expr[WarpScript[T]](TestIdTransformer.transform(ctx.untypecheck(script.tree)))
   }
 
 
+
+  /**
+    *
+    * @param ctx blackbox macro Context
+    * @return fully qualified class name of the enclosing runtime class.
+    */
   def extractEnclosingClass(ctx: blackbox.Context): String = {
     import ctx.universe._
     ctx.reifyEnclosingRuntimeClass match {
+      // 2.13
       case Literal(Constant(className)) => className.toString
+      // 2.11, 2.12
       case t@TypeApply(_, _) =>
-        val pattern = """(Predef\.this\.)?classOf\[(.+)\]""".r
+        val pattern: Regex = """(Predef\.this\.)?classOf\[(.+)\]""".r
         val pattern(_, cls) = t.toString
         cls
       case other => throw new RuntimeException(s"""we can only extract a class name from a reified enclosing runtime class: ${showRaw(other)}""")
@@ -93,8 +108,8 @@ object Macros {
   /**
     * Extracts a named argument from a function tree.
     *
-    * ie, given a tree representing ((a: IU
-    * @param ctx
+    * ie, given a tree representing ((a: Int) => a + 1), returns "a"
+    * @param ctx blackbox macro Context
     * @param tree
     * @return
     */
@@ -102,7 +117,7 @@ object Macros {
     import ctx.universe._
     // TODO check what happens when _ is used in WarpScript instead of a variable name
     tree match {
-      case Function(List(ValDef(Modifiers(_), TermName(name), _, _)), _) => s"""${extractEnclosingClass(ctx)}.$name"""
+      case Function(List(ValDef(Modifiers(_), TermName(name), _, _)), _) => name
       case _ => throw new RuntimeException("super hacky but we can only extract a test id from a function")
     }
   }
