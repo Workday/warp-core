fee692ed640d4c44c20890b94e25be7c9d73cc5a
tomas mccandless <tomas.mccandless@workday.com>
2023-09-28 11:47:21 -0700

    no default value for spike filter warp properties

---
+++ warp-core/src/test/scala/com/workday/warp/arbiters/ArbiterLikeSpec.scala
@@ -0,0 +1,36 @@
+package com.workday.warp.arbiters
+
+import com.workday.warp.TestId
+import com.workday.warp.junit.{UnitTest, WarpJUnitSpec}
+import com.workday.warp.persistence.Tables._
+import com.workday.warp.persistence.Tables.RowTypeClasses._
+import com.workday.warp.persistence.TablesLike._
+import com.workday.warp.persistence.TablesLike.RowTypeClasses._
+
+import java.time.Instant
+import java.util.UUID
+
+class ArbiterLikeSpec extends WarpJUnitSpec with ArbiterLike {
+
+  @UnitTest
+  def readSpikeFilterSettings(): Unit = {
+    val methodSignature = s"com.workday.warp.arbiters.${UUID.randomUUID.toString}"
+    val testId = TestId.fromString(methodSignature)
+    val testExec: TestExecutionRowLike = this.persistenceUtils.createTestExecution(testId, Instant.now(), 5.0, 6.0)
+
+    val settingsRow = SpikeFilterSettingsRow(testExec.idTestDefinition, false, 10, 10)
+    this.persistenceUtils.writeSpikeFilterSettings(Seq(settingsRow))
+
+    this.spikeFilterSettings(testExec) should be (settingsRow.spikeFilterEnabled, settingsRow.alertOnNth)
+  }
+
+  /**
+    * Checks that the measured test passed its performance requirement. If the requirement is failed, constructs an
+    * error with a useful message wrapped in an Option.
+    *
+    * @param ballot        box used to register vote result.
+    * @param testExecution [[TestExecutionRowLikeType]] we are voting on.
+    * @return a wrapped error with a useful message, or None if the measured test passed its requirement.
+    */
+  override def vote[T: TestExecutionRowLikeType](ballot: Ballot, testExecution: T): Option[Throwable] = None
+}
